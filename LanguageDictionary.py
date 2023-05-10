92c9b0566a3bf7e02a736f515918aea48453b87e
andrea nisco <andreanisco@gmail.com>
2023-05-10 18:15:39 +0200

    Creazione file LanguageDictionary

---
+++ LanguageDictionary.py
@@ -0,0 +1,20 @@
+translations = {
+    "english": {
+        "interface_life": "Life",
+        "interface_strenght": "Strenght",
+        "interface_mana": "Mana",
+        "interface_gold": "Gold",
+    },
+    "italian": {
+        "interface_life": "Vita",
+        "interface_strenght": "Forza",
+        "interface_mana": "Mana",
+        "interface_gold": "Oro",
+    }
+}
+
+"""
+selected_language = "italian"
+str = translations[selected_language]["interface_strenght"]
+print(str)  # output: "Forza"
+"""
