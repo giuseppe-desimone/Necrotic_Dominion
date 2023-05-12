b7949ce8bc1bb8c359677f5eaece3c7bfabeb82b
giuseppe-desimone <giuseppe.ds.196@gmail.com>
2023-05-12 19:39:26 +0200

    Fixed some bugs and room description translations

--- lv_1/map_l1.py
+++ lv_1/map_l1.py
@@ -1,5 +1,5 @@
 """v0.12"""
-
+from interface import *
 
 class Stanza:
     def __init__(self, X, Y, description, objects=None):
@@ -17,14 +17,14 @@ mappa_l1 = [[Stanza(X=i, Y=j, description="descrizione", objects=[]) for j in ra
 
 # Aggiunta di alcune stanze alla mappa
 
-mappa_l1[0][3].description = "Nel cuore del castello si trova l’armeria, una vasta stanza dalle pareti in pietra ornate da \nantichi stemmi e scudi impolverati. \n\nUna volta custode di migliaia di armi e armature, ora le grandi mensole e gli armadi dell’armeria \nrimangono vuoti e silenziosi. \n\nUn’indicibile tristezza si respira nell’aria, interrotta solo dalla vista di una cassapanca in un \nangolo."
-mappa_l1[0][4].description = "L’atrio del castello è spazioso, \ncon alti soffitti affrescati e \npareti di pietra che riflettono\nla luce. \n\nL’odore di fumo delle torce e di \ncera delle candele riempie \nl’aria, mentre i passi echeggiano \nsui pavimenti di marmo scuro. \n\nAi lati dell’atrio si trovano \ngrandi porte di quercia scolpite \na mano, una rivolta verso est e \nl’altra verso ovest. \n\nUn ampio corridoio si apre verso \nsud, con i lampadari appesi alle \npareti di pietra che illuminano \nil percorso e creano ombre \ndanzanti."
-mappa_l1[0][5].description = "Ti trovi in una stanza vuota e desolata, senza finestre né mobili. \n\nLa tua unica via di uscita è una porta a ovest, mentre a est c'è una serratura. \n\nL'aria è silenziosa e immobile, creando un'atmosfera inquietante e misteriosa."
-mappa_l1[0][6].description = "Al centro del salone buio si trova la fontana sacra, che emana una flebile luce blu e si avvolge in una foschia misteriosa. \n\nL'acqua che sgorga da essa è corrotta dalle anime dei sacrifici e nutre antiche divinità oscure, \noffrendo un potere oltre la comprensione umana a coloro che osano avvicinarsi."
-mappa_l1[1][4].description = "Ti trovi in un sontuoso corridoio dal pavimento di marmo, illuminato da lampadari pendenti. \n\nSulle pareti si trovano quadri di uomini dall'espressione severa che sembrano scrutarti. \n\nL'atmosfera è solenne e formale, \nma anche un po' intimidatoria. \n\nIl corridoio sembra non avere fine e non si vedono porte né finestre, come se fosse un labirinto senza\nvia d'uscita."
-mappa_l1[2][4].description = "Hai raggiunto la fine del corrido-io, dove trovi due porte. \n\nUna porta chiusa si trova a sud, \nsenza alcuna indicazione su cosa \npossa trovarsi dall'altra parte. \n\nLa seconda porta è ad est e non si può vedere attraverso la \nserratura. \n\nL'atmosfera è ancora solenne e \nmisteriosa, lasciando intendere che \nci possano essere segreti o \npericoli in agguato oltre le \nporte."
-mappa_l1[2][5].description = "Entri in una stanza del castello \natipica. Sembra che sia stata \nriadattata a rifugio, con un \ngiaciglio per dormire, e un \nsemplice tavolo in legno. Una \ncandela accesa sul tavolo rompe \nil buio della stanza."
-mappa_l1[3][4].description = "Sei in una stanza fredda, con delle scale"
+mappa_l1[0][3].description = translations["l1_03"][i.lang]
+mappa_l1[0][4].description = translations["l1_04"][i.lang]
+mappa_l1[0][5].description = translations["l1_05"][i.lang]
+mappa_l1[0][6].description = translations["l1_06"][i.lang]
+mappa_l1[1][4].description = translations["l1_14"][i.lang]
+mappa_l1[2][4].description = translations["l1_24"][i.lang]
+mappa_l1[2][5].description = translations["l1_25"][i.lang]
+mappa_l1[3][4].description = translations["l1_34"][i.lang]
 
 ########################################################################################################################
 
